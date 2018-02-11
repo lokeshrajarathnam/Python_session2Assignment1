6deb7f0a0a3ff427c29ae07f34831ac23089c38b
lokeshrajarathnam <36074007+lokeshrajarathnam@users.noreply.github.com>
2018-02-11 16:44:05 +0530

    User input to List object

---
+++ stringTolist.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 11 15:35:59 2018
+
+@author: lokesh.r
+"""
+
+astr = input("enter input string as comma seperated =>")
+alist = astr.split(',')
+alist.insert(0,"tommy")
+#alist.reverse()
+print(alist)
