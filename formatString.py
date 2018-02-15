2b707b3431d9b2df6b4f16af88b90bd22a40c1df
lokeshrajarathnam <36074007+lokeshrajarathnam@users.noreply.github.com>
2018-02-15 13:51:28 +0530

    Add files via upload

---
+++ formatString.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 15 13:42:53 2018
+
+@author: lokesh.r
+"""
+
+str = "WE, THE PEOPLE OF INDIA,\n\thaving solemnly resolved to constitute India into a"+"SOVEREIGN,"+"!\n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC"+"\n\t\tand to secure to all its citizens"
+
+print(str)
